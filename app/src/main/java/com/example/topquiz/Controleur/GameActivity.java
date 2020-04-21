c84ea0da8b15ca88c95024952ee26f06b7f8e26a
lucafrigeni1 <jean.frigeni@gmail.com>
2020-04-21 10:25:32 +0200

    l'app fonctionne

--- app/src/main/java/com/example/topquiz/Controleur/GameActivity.java
+++ app/src/main/java/com/example/topquiz/Controleur/GameActivity.java
@@ -1,6 +1,5 @@
 package com.example.topquiz.Controleur;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -8,7 +7,6 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
-import android.os.PersistableBundle;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
